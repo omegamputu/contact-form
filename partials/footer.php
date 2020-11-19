b44cc223c405e78af870a514f69a021dbfe6e7c2
Omega Mputu <kutekemeni93@gmail.com>
2020-11-19 00:37:20 -0800

    Ajout d'un favicon

--- partials/footer.php
+++ partials/footer.php
@@ -1,8 +1,7 @@
 <!-- JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script src="js/script.js"></script>
+   
   </body>
 </html>
 
